38b745d16915c524d8bc86c3ffd22035d2f21fa0
jassimalfonso <jassim.alfonso@gmail.com>
2023-09-22 21:01:43 +0400

    Updated markup, styling and script

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const logo = document.querySelector(".logo");
 const banner = document.querySelector(".banner");
 
 logo.addEventListener("click", () => {
-	logo.classList.toggle("fade-out");
-	banner.classList.toggle("fade-in");
+	logo.classList.add("fade-out");
+	logo.classList.remove("fade-in");
+	banner.classList.add("fade-in");
+	banner.classList.remove("fade-out");
+});
+
+banner.addEventListener("click", () => {
+	logo.classList.add("fade-out");
+	logo.classList.add("fade-in");
+	banner.classList.add("fade-in");
+	banner.classList.add("fade-out");
 });
